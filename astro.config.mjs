307b6d1118b13f3668e58cba4b5fdb3ca9cd597f
Christophe anfry <frybcn@gmail.com>
2023-12-05 14:19:09 +0100

    Add Tailwind CSS configuration and Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
+});

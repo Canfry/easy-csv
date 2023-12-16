f5fac16f4d03fcc48bb7648f3985a19011d563c0
Christophe anfry <frybcn@gmail.com>
2023-12-16 22:08:40 +0100

    Refactor file upload logic and add file listing in dashboard

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,33 +1,8 @@
-import fs from 'fs';
-import csv from 'csv-parser';
-
 import type { APIRoute } from 'astro';
 import { XataClient } from '../../xata';
 import { XataFile } from '@xata.io/client';
 
-// export const GET: APIRoute = async ({ redirect }) => {
-//   const xata = new XataClient({ apiKey: import.meta.env.XATA_API_KEY });
-
-//   const records = await xata.db.files
-//     .select([
-//       'id',
-//       'file.id',
-//       'file.name',
-//       'file.signedUrl',
-//       'file.mediaType',
-//       'file.size',
-//       'file.enablePublicUrl',
-//       'file.url',
-//       'user.id',
-//       'user.name',
-//       'user.email',
-//       'user.password',
-//     ])
-//     .getAll();
-
-//   console.log(records);
-//   return redirect('/dashboard');
-// };
+const errors: Record<string, string> = {};
 
 export const POST: APIRoute = async ({ redirect, cookies, request }) => {
   const xata = new XataClient({ apiKey: import.meta.env.XATA_API_KEY });
@@ -36,19 +11,23 @@ export const POST: APIRoute = async ({ redirect, cookies, request }) => {
   const fileToUpload = data.get('file') as File;
   const fileName = fileToUpload.name;
 
-  console.log(fileToUpload.name);
+  console.log(fileToUpload);
 
-  const record = Buffer.from(await fileToUpload.arrayBuffer()).toString(
-    'base64'
-  );
-  console.log(record);
+  if (!fileToUpload) {
+    errors.file = 'File is required';
+  } else {
+    const record = Buffer.from(await fileToUpload.arrayBuffer()).toString(
+      'base64'
+    );
+    console.log(record);
 
-  await xata.db.files.create({
-    name: fileName,
-    user: userId?.value,
-    file: XataFile.fromBase64(record),
-  });
+    await xata.db.files.create({
+      name: fileName,
+      user: userId?.value,
+      file: XataFile.fromBase64(record),
+    });
 
-  // console.log(records);
-  return redirect('/dashboard');
+    // console.log(records);
+    return redirect('/dashboard');
+  }
 };

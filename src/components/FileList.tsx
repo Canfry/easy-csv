22b7b2e865d73a138b2c334ed460b80aeff66686
Christophe anfry <frybcn@gmail.com>
2023-12-26 10:44:42 +0100

    Add + button and delete button in fileList

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -3,6 +3,7 @@ import type Data from '../pages/dashboard/index.astro';
 import type { FilesRecord } from '../xata';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { IoTrashSharp } from 'react-icons/io5';
 
 export default function FileList({
   files,
@@ -37,17 +38,62 @@ export default function FileList({
   return (
     <div className='grid grid-cols-[auto,1fr] w-full z-50'>
       <div className='py-16 px-8 bg-gray-50 flex flex-col gap-y-4'>
-        <button className='text-center rounded-full w-8 h-8 bg-orange-600 flex items-center justify-center mx-auto pb-1 cursor-pointer'>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <button
+          className='text-center rounded-full w-8 h-8 bg-slate-600 flex items-center justify-center mx-auto pb-1 cursor-pointer'
+          onClick={() => document.getElementById('my_modal_1')?.showModal()}
+        >
           <p className='text-3xl text-white'>+</p>
         </button>
+        <dialog id='my_modal_1' className='modal'>
+          <div className='modal-box'>
+            <h3 className='font-bold text-lg'>Upload file!!</h3>
+            <p className='py-4'>
+              Press ESC key or click the button below to close
+            </p>
+            <form
+              className='flex items-center gap-x-6 max-md:flex-col max-md:gap-y-6 my-4'
+              method='POST'
+              action='/api/upload'
+              encType='multipart/form-data'
+            >
+              <input
+                type='file'
+                name='file'
+                id='file'
+                required
+                className='file-input file-input-bordered border-orange-600 file-input-[orange-600] w-full max-w-xs my-4'
+              />
+              <button
+                type='submit'
+                className='border border-slate-600 bg-transparent text-orange-600 py-2.5 px-4 rounded-md hover:bg-orange-600 hover:border-orange-600 hover:text-white'
+              >
+                Upload
+              </button>
+            </form>
+            <div className='modal-action'>
+              <form method='dialog'>
+                {/* if there is a button in form, it will close the modal */}
+                <div className='flex items-center gap-x-4'>
+                  <button className='btn'>Cancel</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </dialog>
+        {/* <button className='text-center rounded-full w-8 h-8 bg-orange-600 flex items-center justify-center mx-auto pb-1 cursor-pointer'>
+          <p className='text-3xl text-white'>+</p>
+        </button>*/}
         {files.map((file) => (
-          <button
-            className='text-orange-600 font-bold'
-            key={file.id}
-            onClick={() => getData(file?.name ?? '')}
-          >
-            {file.name}
-          </button>
+          <div key={file.id} className='flex items-center gap-x-2'>
+            <button
+              className='text-orange-600 font-bold'
+              onClick={() => getData(file?.name ?? '')}
+            >
+              {file.name}
+            </button>
+            <IoTrashSharp className='text-slate-600 cursor-pointer' />
+          </div>
         ))}
       </div>
       <div className='overflow-x-scroll w-full'>

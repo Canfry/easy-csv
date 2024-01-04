720ad55475ad00a2524e8f5ec595331da9fd765d
Christophe anfry <frybcn@gmail.com>
2024-01-04 15:17:51 +0100

    Remove tunnel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 
-import tunnel from 'astro-tunnel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), tunnel()],
+  integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel(),
   prefetch: false,

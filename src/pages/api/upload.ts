fb4a1c8536efcf7d7ab1f9c91865205f510fdc9e
Christophe anfry <frybcn@gmail.com>
2024-01-08 14:15:47 +0100

    add all

---
+++ src/pages/api/upload.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from 'astro';
+import { XataClient } from '../../xata';
+import { XataFile } from '@xata.io/client';
+
+const errors: Record<string, string> = {};
+
+export const POST: APIRoute = async ({ redirect, cookies, request }) => {
+  const xata = new XataClient({ apiKey: import.meta.env.XATA_API_KEY });
+  const userId = cookies.get('userId');
+  const data = await request.formData();
+  const fileToUpload = data.get('file') as File;
+  const fileName = fileToUpload.name;
+
+  // console.log(fileToUpload);
+
+  if (!fileToUpload) {
+    errors.file = 'File is required';
+  } else {
+    const record = Buffer.from(await fileToUpload.arrayBuffer()).toString(
+      'base64'
+    );
+    // console.log(record);
+
+    await xata.db.files.create({
+      name: fileName,
+      user: userId?.value,
+      file: XataFile.fromBase64(record),
+    });
+    return redirect('/dashboard');
+  }
+};

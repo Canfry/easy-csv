8f199e10816c1627c32e0f822afb11cf3666e038
Christophe anfry <frybcn@gmail.com>
2024-01-08 13:35:30 +0100

    change to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
-// import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vercel()],
+  integrations: [tailwind(), react(), netlify()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   prefetch: false,
   ssr: {
     noExternal: ['react-icons'],

936d557ee2a2c982a0b271d077d1e63c8788f9aa
Christophe anfry <frybcn@gmail.com>
2024-01-09 15:03:54 +0100

    Add type to error

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -24,7 +24,7 @@ export const POST: APIRoute = async ({ redirect, cookies, request }): Promise<Re
         file: XataFile.fromBase64(record),
       })
     }
-  } catch (error) {
+  } catch (error: string) {
     throw new Error({message: error.message});
   }
 

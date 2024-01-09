8d5d784e833c83657d9612e20b63061796f61a4c
Christophe anfry <frybcn@gmail.com>
2024-01-09 09:48:55 +0100

    Change to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), netlify()],
   output: 'server',
-  adapter: netlify(),
+  adapter: vercel(),
   prefetch: false,
   vite: {
     ssr: {

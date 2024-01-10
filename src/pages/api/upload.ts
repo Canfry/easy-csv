4ae746d0768ba356998676ae645e15d55d053ad8
Christophe anfry <frybcn@gmail.com>
2024-01-10 09:48:17 +0100

    Add new Response in else statement

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -23,9 +23,14 @@ export const POST: APIRoute = async ({ redirect, cookies, request }): Promise<Re
         user: userId?.value,
         file: XataFile.fromBase64(record),
       })
+      return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
-  } catch (error) {
-    return new Response(null, { status: 404, statusText: 'Not Found' });  
+  } catch (error: any) {
+    throw new Error({message: error.message});
   }
 
   return redirect('/dashboard')

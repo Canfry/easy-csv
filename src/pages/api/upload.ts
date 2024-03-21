96408e2d975d04f58caba3411042d58ed73b074f
Christophe anfry <66621302+Canfry@users.noreply.github.com>
2024-03-21 08:10:54 +0100

    Delete comments

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -14,7 +14,6 @@ export const POST: APIRoute = async ({ redirect, cookies, request }): Promise<Re
   /* console.log(fileToUpload); */
   
 
-  /* try { */
     if(!fileToUpload) {
       throw new Error("File is required")
     }
@@ -28,23 +27,9 @@ export const POST: APIRoute = async ({ redirect, cookies, request }): Promise<Re
         user: userId?.value,
         file: XataFile.fromBase64(record),
       })
-      // return new Response(JSON.stringify({ success: true }), {
-      //   status: 200,
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      // })
     }
 
   return redirect('/dashboard');
-
-  // }catch (error) {
-  //   let message;
-  //   if(error instanceof Error) {
-  //     message = error.message;
-  //   }
-  // }
-
 };
 
 
